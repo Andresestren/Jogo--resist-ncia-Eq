edb15f1f3b498297a451d0b7be61067d75e477d3
Andresestren <131630105+Andresestren@users.noreply.github.com>
2026-07-13 20:03:22 -0300

    Delete jogo_resitores directory
    deletando para adicionar arquivos mais recentes

--- jogo_resitores/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/jogo_resitores.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for jogo_resitores.
-# This may be replaced when dependencies are built.
--- jogo_resitores/resitor.cpp
+++
@@ -1,4 +0,0 @@
-#include "resistor.h"
-
-const double RandomResistor::VALUES[] = { 330.0, 100.0, 200.0 };
-const int RandomResistor::VCOUNT = 3;
