259d98c1d2480cbc9958c956c79b26d546dfa067
Andresestren <131630105+Andresestren@users.noreply.github.com>
2026-07-13 19:50:58 -0300

    Delete Projeto Jogo de resistencia QT directory

--- Projeto Jogo de resistencia QT/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_resistors_FILE C:/Users/Allan/Desktop/backup Prog/resistors/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/resistors.exe)
-set(__QT_DEPLOY_TARGET_resistors_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_resistors_RUNTIME_DLLS C:/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
